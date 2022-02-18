1ccf006f7a4618813289c23defca8357dc18825d
errortime <larb26656@gmail.com>
2022-02-19 00:48:40 +0700

    Feat (init  project)

---
+++ src/main/java/com/example/api101withspringboot/Api101WithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.api101withspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api101WithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Api101WithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/api101withspringboot/Api101WithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api101withspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api101WithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
